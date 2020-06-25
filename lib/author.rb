4c4d5a429dee3a9e9a961f93a06f297550cb88f0
Seth Near <seth.j.near@gmail.com>
2020-06-25 14:08:43 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,26 @@
 class Author
 
+attr_accessor :name
 
+def initialize(name)
+  @name = name
+end
 
+def add_post(post)
+  post.artist = self
+end
+
+def add_post_by_name(name)
+  post = Song.new(name)
+  add_post(post)
+end
+
+def posts
+ Song.all.select {|post| post.artist == self}
+end
+
+def self.post_count
+  return Song.all.size
+end
 
 end
